5ef840b65ca49a40e7365d4b5bf5c19b2076225f
Rodney Barnes <rodney@barnes.codes>
2022-03-20 20:25:20 -0400

    Add Timeline component

---
+++ src/lib/components/Timeline.svelte
@@ -0,0 +1,3 @@
+<div class="flex items-center justify-center flex-grow bg-slate-800">
+    <p class="text-white">Timeline coming soon!</p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import Header from '$lib/components/Header.svelte';
+    import Timeline from '$lib/components/Timeline.svelte';
 </script>
 
-<Header />
+<div class="flex flex-col h-screen">
+    <Header/>
+    <Timeline/>
+</div>
+
